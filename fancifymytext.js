2ce378d82d1c5498cbe9b618811497476a3d4466
joshuaLSchool <125104773+joshuaLSchool@users.noreply.github.com>
2024-12-06 23:24:51 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,6 @@
+function biggerFunction() {
+	console.log("biggerFunction triggered");
+	alert("You clicked the button!");
+	let getBigger = document.getElementById("getbigger");
+	getBigger.style.fontSize += 2;
+}
